a9e74f1ef76c8aea26853294c479afb24c610340
ericminio <emignot@change.org>
2023-04-14 16:14:53 -0700

    better test data

--- body_based_proxy/tests.sh
+++ body_based_proxy/tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 function test_when_not_matching {
-    body=$(curl --header "Content-Type: text/plain" --request POST --data 'unknown' http://localhost)
+    body=$(curl --header "Content-Type: text/plain" --request POST --data 'this body will be ignored' http://localhost)
     
     assertequals "$body" "not matching"
 }
 
 function test_when_matching {
-    body=$(curl --header "Content-Type: text/plain" --request POST --data 'matching' http://localhost)
+    body=$(curl --header "Content-Type: text/plain" --request POST --data 'we want this one' http://localhost)
     
     assertequals "$body" "matching"
 }
