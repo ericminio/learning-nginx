be84709e66577d0396e23a84bfab4a7260c52cfb
ericminio <eric.mignot@gmail.com>
2023-04-15 15:57:48 -0700

    try again

--- body_based_proxy/tests.sh
+++ body_based_proxy/tests.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-function test_matching {
+function test_defaults_to_not_matching_with_empty_matching_file {
+    body=$(curl --header "Content-Type: text/plain" --request POST --data 'random payload' http://localhost)
+    
+    assertequals "$body" "not matching"
+}
+
+function test_matching_requires_configuration {
     echo "'~this one' 'matching';" > ./body_based_proxy/nginx/conf.d/matching
     docker exec -it nginx nginx -s reload
-    sleep 1
+    sleep 3
     body=$(curl --header "Content-Type: text/plain" --request POST --data 'we want this one' http://localhost)
     
     assertequals "$body" "matching"
 }
 
-function test_matching_file_welcomes_several_entries {
+function test_matching_configuration_file_welcomes_several_entries {
     echo "'~this one' 'matching';" > ./body_based_proxy/nginx/conf.d/matching
     echo "'~that\s+too' 'matching';" >> ./body_based_proxy/nginx/conf.d/matching
     docker exec -it nginx nginx -s reload
-    sleep 1
+    sleep 3
     body=$(curl --header "Content-Type: text/plain" --request POST --data 'we want that     too' http://localhost)
     
     assertequals "$body" "matching"
 }
 
-function test_not_matching {
-    echo "'~maybe' 'matching';" > ./body_based_proxy/nginx/conf.d/matching
+function test_not_matching_can_also_be_configured {
+    echo "'~maybe' 'not_matching';" > ./body_based_proxy/nginx/conf.d/matching
     docker exec -it nginx nginx -s reload
-    sleep 1
-    body=$(curl --header "Content-Type: text/plain" --request POST --data 'but no' http://localhost)
+    sleep 3
+    body=$(curl --header "Content-Type: text/plain" --request POST --data 'maybe' http://localhost)
     
     assertequals "$body" "not matching"
 }
@@ -31,8 +37,9 @@ function test_not_matching {
 function test_resists_empty_matching_file {
     echo "" > ./body_based_proxy/nginx/conf.d/matching
     docker exec -it nginx nginx -s reload
-    sleep 1
+    sleep 3
     body=$(curl --header "Content-Type: text/plain" --request POST --data 'this is fine' http://localhost)
     
     assertequals "$body" "not matching"
 }
+
