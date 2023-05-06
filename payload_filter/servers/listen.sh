0024b9b553787da7677bb569b32e1b8d78615735
ericminio <emignot@change.org>
2023-05-06 15:01:44 -0700

    pong with content-length header

--- payload_filter/servers/listen.sh
+++ payload_filter/servers/listen.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+length=`echo "$PONG" | wc -c`
+
 file=/usr/local/src/answer
 echo "HTTP/1.1 200 OK" > $file
 echo "Content-Type: text/html; charset=UTF-8" >> $file
+echo "Content-Length: $length" >> $file
 echo "Server: netcat" >> $file
 echo "" >> $file
 echo "$PONG" >> $file
 echo "" >> $file
-answer=`cat $file`
 
+answer=`cat $file`
 echo "listening on port: $PORT"
 echo "answering with:"
 echo "$answer"
