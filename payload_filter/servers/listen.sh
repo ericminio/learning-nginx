aec705cce6c4949a89d0adf207bc684188fc66c5
ericminio <emignot@change.org>
2023-05-06 14:34:02 -0700

    leaner

--- payload_filter/servers/listen.sh
+++ payload_filter/servers/listen.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-message=(
-    "HTTP/1.1 200 OK"
-    "Content-Type: text/html; charset=UTF-8"
-    "Server: netcat"
-    ""
-    "$PONG"
-    ""
-)    
+echo "HTTP/1.1 200 OK" > /usr/local/src/answer
+echo "Content-Type: text/html; charset=UTF-8" >> /usr/local/src/answer
+echo "Server: netcat" >> /usr/local/src/answer
+echo "" >> /usr/local/src/answer
+echo "$PONG" >> /usr/local/src/answer
+echo "" >> /usr/local/src/answer
+answer=`cat /usr/local/src/answer`
 
 echo "listening on port: $PORT"
-echo "answering with: $PONG"
+echo "answering with:"
+echo "$answer"
 
 while true; 
 do 
-    printf "%s\n" "${message[@]}" | nc -lp $PORT; 
+    printf "%s\n" "$answer" | nc -lp $PORT; 
 done
