345460ddff66cd8d1d468ddeb75ff37f49807b7c
ericminio <emignot@change.org>
2023-05-06 15:07:07 -0700

    disclose text/plain correctly

--- payload_filter/servers/listen.sh
+++ payload_filter/servers/listen.sh
@@ -4,7 +4,7 @@ length=`echo "$PONG" | wc -c`
 
 file=/usr/local/src/answer
 echo "HTTP/1.1 200 OK" > $file
-echo "Content-Type: text/html; charset=UTF-8" >> $file
+echo "Content-Type: text/plain; charset=UTF-8" >> $file
 echo "Content-Length: $length" >> $file
 echo "Server: netcat" >> $file
 echo "" >> $file
