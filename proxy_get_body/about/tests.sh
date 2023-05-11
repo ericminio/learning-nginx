8cc493d088992ef494d18bc0953e1a474a421b3c
ericminio <emignot@change.org>
2023-05-11 14:51:46 -0700

    lighter

--- proxy_get_body/about/tests.sh
+++ proxy_get_body/about/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function test_can_proxy_get_payload {
-    body=$(curl --header "Content-Type: text/plain" --request GET --data 'can you see me?' http://localhost:8080)
+    body=$(curl --request GET --data 'can you see me?' http://localhost:8080)
     
     assertequals "$body" "GET: can you see me? I can see you"
 }
