cbcf262afba44f3d8e63ad1607f69c8b7da9f479
ericminio <eric.mignot@gmail.com>
2023-05-12 06:35:08 -0700

    proxy_set_header

--- proxy_set_header/about/tests.sh
+++ proxy_set_header/about/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function test_proxy_set_header {
-    body=$(curl http://localhost/api/ping)
+    body=$(curl http://localhost:8080/api/ping)
     
     assertequals "$body" "you called /ping with header x-answer:42"
 }
